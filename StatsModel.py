f20a39baade5b48b1f0a29375b3c2c7b899eaa2b
unknown <yakinoki>
2023-01-25 00:17:10 +0900

    add_stmodel

--- StatsModel.py
+++ StatsModel.py
@@ -6,8 +6,16 @@ import optuna as op
 import itertools
 import pandas as pd
 import numpy as np
+from datetime import datetime
 
 
-# データの読み込み
-bill = pd.read_csv("sample.csv",dtype={'x02':'float64'})
+# 日付形式で読み込む
+dateparse = lambda dates: pd.datetime.strptime(dates, '%Y/%m/%d')
+data = pd.read_csv('test_data.csv', index_col='ds', date_parser=dateparse, dtype='float')
 
+
+# ローカルレベルモデルによる状態推定
+model = sm.tsa.UnobservedComponents(data, 'local level')
+result = model.fit()
+fig = result.plot_components()
+print(result.summary())
