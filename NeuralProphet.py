0fd2d97c787b29f54faca2a19d4459da6e864a90
unknown <yakinoki>
2023-04-08 13:56:42 +0900

    add_neuralprophet

--- NeuralProphet.py
+++ NeuralProphet.py
@@ -1,20 +1,17 @@
 import pandas as pd
-import warnings
-from prophet import Prophet
-
-warnings.filterwarnings("ignore")
+from neuralprophet import NeuralProphet
 
 # データの読み込み
 df_train = pd.read_csv('dataset/raw_data.csv')
 df_train.columns = ['ds', 'y']
 
-model = Prophet()
+model = NeuralProphet()
 model.fit(df_train)
 #df_testの最後の日付から何日間分未来の予測値を出すか。
-future = model.make_future_dataframe(periods=60)
+future = model.make_future_dataframe(df_train, periods=60)
 forecast = model.predict(future)
 #予測の最終日から何日分出力するか。
-result = forecast[['ds', 'yhat']].tail(30)
+result = forecast[['ds', 'yhat1']].tail(30)
 
 
 # 予測結果を出力
