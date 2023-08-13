302d86f916b3f78454e514aafb24ee7d4fedfd4a
NAOYA\bundl <yakinoki>
2023-08-13 10:22:56 +0900

    add_params

--- Prophet_3.py
+++ Prophet_3.py
@@ -17,7 +17,21 @@ df_train = pd.merge(df_train, df_events, on='ds', how='left')
 # イベントフラグの欠損値を0で埋める
 df_train['event_flag'] = df_train['event_flag'].fillna(0)
 
-model = Prophet()
+# パラメータの辞書を作成
+params = {
+    'growth': 'linear',  # 成長モデル
+    # 'changepoints': ['2023-01-01', '2023-04-01'],  # トレンド変化点の指定
+    'n_changepoints': 25,  # トレンド変化点の数
+    'seasonality_mode': 'additive',  # 季節性モデル（加法モデル）
+    'holidays': None,  # 祝日や特別なイベント
+    'seasonality_prior_scale': 10.0,  # 季節性の強さ
+    'holidays_prior_scale': 10.0,  # 祝日の影響
+    'changepoint_prior_scale': 0.05,  # トレンド変化点の影響
+    'daily_seasonality': True  # 曜日に対する季節性をモデル化する
+}
+
+# Prophetモデルのインスタンスを作成
+model = Prophet(**params)
 
 # イベントフラグを追加
 model.add_regressor('event_flag')
